300c0bd0d080dd3cbf3a666192e30ef5e4909418
Uladzislau Komar <44633680+NotRealKomar@users.noreply.github.com>
2023-05-04 14:47:28 +0300

    Make PoC crawler (#2)

---
+++ modules/crawler/CrawlerService.go
@@ -0,0 +1,79 @@
+package crawler
+
+import (
+	"net/url"
+	"strings"
+	"web-crawler/modules/documents"
+	"web-crawler/modules/elastic/repositories"
+	"web-crawler/modules/http"
+	"web-crawler/modules/logger"
+	"web-crawler/modules/parser"
+)
+
+const MAX_DEPTH = 3
+const MAX_LINK_COUNT = 5
+
+type CrawlerService struct {
+	contentRepository repositories.ContentRepository
+	httpClient        http.HttpClientService
+	parserService     parser.ParserService
+}
+
+func NewCrawlerService(
+	contentRepository repositories.ContentRepository,
+	httpClient http.HttpClientService,
+	parserService parser.ParserService,
+) *CrawlerService {
+	return &CrawlerService{
+		contentRepository,
+		httpClient,
+		parserService,
+	}
+}
+
+func (service *CrawlerService) InitializeCrawl(url *url.URL) error {
+	return service.crawl(url, 0, 0)
+}
+
+func (service *CrawlerService) crawl(url *url.URL, linkCount int, depth int) error {
+	logger.Log("Start crawling on depth", depth, "in", url.String())
+
+	if depth > MAX_LINK_COUNT {
+		logger.Log("Link count", linkCount, "in depth", depth, "exceeded max link count in", url.String())
+		return nil
+	}
+
+	if depth > MAX_DEPTH {
+		logger.Log("Depth", depth, "exceeded max depth in", url.String())
+		return nil
+	}
+
+	response, getErr := service.httpClient.Get(url)
+	if getErr != nil {
+		return getErr
+	}
+
+	parseData, parseErr := service.parserService.Parse(*response)
+	if parseErr != nil {
+		return parseErr
+	}
+
+	document := documents.NewContentDocument()
+	document.Source = url.String()
+	document.Data = strings.Join(parseData.Data, " ")
+
+	service.contentRepository.Save(*document)
+
+	depth += 1
+
+	for idx, link := range parseData.Links {
+		if idx <= MAX_LINK_COUNT && depth <= MAX_DEPTH {
+			crawlErr := service.crawl(&link, idx, depth)
+			if crawlErr != nil {
+				return crawlErr
+			}
+		}
+	}
+
+	return nil
+}
--- modules/parser/ParserService.go
+++ modules/parser/ParserService.go
@@ -3,24 +3,39 @@ package parser
 import (
 	"errors"
 	"io"
+	"net/url"
+	"regexp"
 	"strings"
 
 	"web-crawler/modules/logger"
 	parserErrors "web-crawler/modules/parser/errors"
 
+	"golang.org/x/exp/slices"
 	"golang.org/x/net/html"
 )
 
+const (
+	HTML_ANCHOR_TAG            = "a"
+	HTML_ANCHOR_HREF_ATTRIBUTE = "href"
+	HTML_PARAGRAPH_TAG         = "p"
+	HTML_SPAN_TAG              = "span"
+	HTML_HEADER_TAG            = "h1"
+
+	REGEX_NEWLINE_EXPRESSION = "(?m)\n {1,}"
+
+	HTTPS_PROTOCOL = "https"
+)
+
 type ParseData struct {
-	Data []string
-	// links []string
+	Data  []string
+	Links []url.URL
 }
 
 type ParseInputReader io.ReadCloser
 type ParserService struct{}
 
-func (*ParserService) Parse(reader ParseInputReader) (output *ParseData, parseError error) {
-	output = &ParseData{}
+func (*ParserService) Parse(reader ParseInputReader) (*ParseData, error) {
+	output := &ParseData{}
 
 	tokenizer := html.NewTokenizer(reader)
 	defer reader.Close()
@@ -38,12 +53,41 @@ func (*ParserService) Parse(reader ParseInputReader) (output *ParseData, parseEr
 			return nil, errors.New(parserErrors.ErrorTokenException)
 		}
 
-		if tokenType == html.TextToken {
-			data := strings.ReplaceAll(tokenizer.Token().Data, "\n", "")
-			data = strings.ReplaceAll(data, "\t", "")
+		if tokenType == html.StartTagToken {
+			token := tokenizer.Token()
 
-			if len(strings.ReplaceAll(data, " ", "")) > 0 {
-				output.Data = append(output.Data, strings.Trim(data, " "))
+			if token.Data == HTML_ANCHOR_TAG {
+				idx := slices.IndexFunc(token.Attr, func(attr html.Attribute) bool {
+					return attr.Key == HTML_ANCHOR_HREF_ATTRIBUTE
+				})
+
+				if idx != -1 {
+					link := token.Attr[idx].Val
+
+					url, err := url.ParseRequestURI(link)
+
+					if err == nil && url.IsAbs() && url.Scheme == HTTPS_PROTOCOL {
+						output.Links = append(output.Links, *url)
+					}
+				}
+			}
+
+			if token.Data == HTML_PARAGRAPH_TAG ||
+				token.Data == HTML_SPAN_TAG ||
+				token.Data == HTML_ANCHOR_TAG ||
+				token.Data == HTML_HEADER_TAG {
+				innerTokenType := tokenizer.Next()
+
+				if innerTokenType == html.TextToken {
+					innerToken := tokenizer.Token()
+					newLineRegexp := regexp.MustCompile(REGEX_NEWLINE_EXPRESSION)
+
+					data := newLineRegexp.ReplaceAllString(strings.TrimSpace(innerToken.Data), " ")
+
+					if len(data) > 0 {
+						output.Data = append(output.Data, data)
+					}
+				}
 			}
 		}
 	}
